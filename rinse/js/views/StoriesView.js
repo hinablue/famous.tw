c92539aa946c8e58b854d1c17e966a1b069d7207
Hina Chen <hinablue@gmail.com>
2014-05-05 11:23:54 +0800

    Use gridLayout in rinse

--- rinse/js/views/StoriesView.js
+++ rinse/js/views/StoriesView.js
@@ -6,12 +6,15 @@ define(function(require, exports, module) {
   var ContainerSurface     = require('famous/surfaces/ContainerSurface');
   var Utility              = require('famous/utilities/Utility');
   var ScrollView           = require('famous/views/Scrollview');
+  var GridLayout           = require('famous/views/GridLayout');
   var SequentialLayout     = require('famous/views/SequentialLayout');
 
   function StoriesView() {
     View.apply(this, arguments);
 
     this.stories = [];
+    this.maskers = [];
+    this._maskerTimer = undefined;
 
     _createStories.call(this);
   }
@@ -23,12 +26,12 @@ define(function(require, exports, module) {
       classes: ['stories-container']
     });
 
-    var storiesCol, storiesRow, _storiesRow = [], surf, mod, container, view, data, masker;
+    var grid = [], surf, mod, container, view, data, masker;
 
-    storiesCol = new SequentialLayout({
+    var storiesContent = new SequentialLayout({
       direction: Utility.Direction.Y
     });
-    storiesCol.sequenceFrom(this.stories);
+    storiesContent.sequenceFrom(grid);
 
     container = new ContainerSurface({
       size: [this.options.width * 2 + this.options.margin, this.options.sloganHeight]
@@ -43,79 +46,88 @@ define(function(require, exports, module) {
     });
     container.add(mod).add(surf);
 
-    this.stories.push(container);
+    mod = new Modifier({
+      origin: [0.5, 0]
+    });
+    this.container.add(mod).add(container);
 
-    for (var i = 0; i < this.options.total / 2; i++) {
-      view = new View();
+    grid.push(container);
 
-      _storiesRow = [];
-      storiesRow = new SequentialLayout({
-        direction: Utility.Direction.X
+    var gridLayout = new GridLayout({
+      dimensions: [2, 5],
+      cellSize: [this.options.width, this.options.height + this.options.margin]
+    });
+    gridLayout.sequenceFrom(this.stories);
+
+    for (var j = 0; j < this.options.total; j++) {
+      container = new ContainerSurface({
+        size: [this.options.width, this.options.height + this.options.margin]
       });
-      storiesRow.sequenceFrom(_storiesRow);
+      surf = new Surface({
+        size: [this.options.width, this.options.height],
+        classes: ['story'],
+        properties: {
+          backgroundImage: 'url(http://placekitten.com/'+this.options.width+'/'+this.options.height+'?image='+j+')'
+        }
+      });
+      container.add(surf);
 
-      for (var j = 0; j < 2; j++) {
-        container = new ContainerSurface({
-          size: [this.options.width + (j === 0 ? this.options.margin : 0), this.options.height + this.options.margin]
-        });
-        surf = new Surface({
-          size: [this.options.width, this.options.height],
-          classes: ['story'],
-          properties: {
-            backgroundImage: 'url(http://placekitten.com/'+this.options.width+'/'+this.options.height+'?image='+(2*i+j)+')'
-          }
-        });
-        container.add(surf);
+      surf = new Surface({
+        size: [this.options.width, this.options.height],
+        classes: ['story-masker']
+      });
+      masker = new Modifier({
+        opacity: 0
+      });
+      this.maskers.push(masker);
+      container.add(masker).add(surf);
 
-        surf = new Surface({
-          size: [this.options.width, this.options.height],
-          classes: ['story-masker']
-        });
-        masker = new Modifier({
-          opacity: 0
-        });
-        container.add(masker).add(surf);
+      data = {
+        author: 'Hina Chen',
+        slogan: 'I\'m Hina.'
+      };
 
-        data = {
-          author: 'Hina Chen',
-          slogan: 'I\'m Hina.'
-        };
+      surf = new Surface({
+        size: [this.options.width / 2, this.options.height / 2],
+        classes: ['story-item-content'],
+        content: '<h3 class="author">'+data.author+'</h3><hr class="short-line"><h2 class="slogan">'+data.slogan+'</h2>'
+      });
+      mod = new Modifier({
+        origin: [0.5, 0.5]
+      });
+      container.add(mod).add(surf);
 
-        surf = new Surface({
-          size: [this.options.width / 2, this.options.height / 2],
-          classes: ['story-item-content'],
-          content: '<h3 class="author">'+data.author+'</h3><hr class="short-line"><h2 class="slogan">'+data.slogan+'</h2>'
-        });
-        mod = new Modifier({
-          origin: [0.5, 0.5]
-        });
-        container.add(mod).add(surf);
+      surf = new Surface({
+        size: [this.options.width, this.options.height],
+        classes: ['story-masker-trigger']
+      });
+      surf.on('mouseenter', function(key, event) {
+        if (this._maskerTimer !== undefined) clearTimeout(this._maskerTimer);
+        this._maskerTimer = setTimeout(function(key) {
+          this.maskers[key].setOpacity(1, this.options.transition);
+        }.bind(this, key), 300);
+      }.bind(this, j));
+      surf.on('mouseleave', function(key, event) {
+        if (this._maskerTimer !== undefined) clearTimeout(this._maskerTimer);
+        this.maskers[key].setOpacity(0, this.options.transition);
+      }.bind(this, j));
+      container.add(surf);
 
-        surf = new Surface({
-          size: [this.options.width, this.options.height],
-          classes: ['story-masker-trigger']
-        });
-        surf.on('mouseenter', function(mod, event) {
-          mod.setOpacity(1, this.options.transition);
-        }.bind(this, masker));
-        surf.on('mouseleave', function(mod, event) {
-          mod.setOpacity(0, this.options.transition);
-        }.bind(this, masker));
-        container.add(surf);
-
-        _storiesRow.push(container);
-      }
-
-      view.add(storiesRow);
+      view = new View();
+      view.add(container);
 
       this.stories.push(view);
     }
 
-    mod = new Modifier({
-      origin: [0.5, 0.5]
+    container = new ContainerSurface({
+      size: [this.options.width * 2 + this.options.margin, (this.options.height + this.options.margin) * (this.options.total / 2)]
     });
+    mod = new Modifier({
+      origin: [0.5, 0.9]
+    });
+    container.add(gridLayout);
 
-    this.container.add(mod).add(storiesCol);
+    this.container.add(mod).add(container);
     this.container.pipe(this.options.scroller);
 
     this.add(this.container);
